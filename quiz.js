9a3d3d38a019a732b8c53aaa81ca74e234ac377d
Relion Florian <29398553+DieuTopGun@users.noreply.github.com>
2025-05-20 15:02:23 +0200

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -93,7 +93,40 @@ const questions = [
   { question: "Quel est le pronom personnel sujet en anglais pour un objet comme 'la voiture' ?", answers: ["he", "she", "it", "they"], correct: 2 },
   { question: "Quel pronom personnel sujet en anglais utilise-t-on pour un animal ou un objet ?", answers: ["it", "he", "we", "they"], correct: 0 },
   { question: "Quel pronom anglais remplace un mot comme 'le soleil' ?", answers: ["she", "they", "he", "it"], correct: 3 },
-  { question: "Quel pronom anglais utilise-t-on pour parler du temps, comme dans 'il pleut' ?", answers: ["it", "he", "they", "she"], correct: 0 }
+  { question: "Quel pronom anglais utilise-t-on pour parler du temps, comme dans 'il pleut' ?", answers: ["it", "he", "they", "she"], correct: 0 },
+
+  //La date
+  { question: "Comment dit-on 'Lundi' en anglais ?", answers: ["Saturday", "Tuesday", "Monday", "Friday"], correct: 2 },
+    { question: "Comment dit-on 'Mardi' en anglais ?", answers: ["Monday", "Thursday", "Friday", "Tuesday"], correct: 3 },
+    { question: "Comment dit-on 'Mercredi' en anglais ?", answers: ["Monday", "Wednesday", "Friday", "Tuesday"], correct: 1 },
+    { question: "Comment dit-on 'Jeudi' en anglais ?", answers: ["Thursday", "Wednesday", "Friday", "Tuesday"], correct: 0 },
+    { question: "Comment dit-on 'Vendredi' en anglais ?", answers: ["Thursday", "Wednesday", "Friday", "Tuesday"], correct: 2 },
+    { question: "Comment dit-on 'Samedi' en anglais ?", answers: ["Thursday", "Wednesday", "Sunday", "Saturday"], correct: 3 },
+    { question: "Comment dit-on 'Dimanche' en anglais ?", answers: ["Thursday", "Sunday", "Friday", "Tuesday"], correct: 1 },
+    { question: "Comment dit-on 'Janvier' en anglais ?", answers: ["June", "January", "Friday", "July"], correct: 1 },
+    { question: "Comment dit-on 'Février' en anglais ?", answers: ["April", "September", "February", "October"], correct: 2 },
+    { question: "Comment dit-on 'Mars' en anglais ?", answers: ["September", "April", "October", "March"], correct: 3 },
+    { question: "Comment dit-on 'Avril' en anglais ?", answers: ["October", "September", "April", "February"], correct: 2 },
+    { question: "Comment dit-on 'Mai' en anglais ?", answers: ["May", "April", "October", "December"], correct: 0 },
+    { question: "Comment dit-on 'Juin' en anglais ?", answers: ["September", "June", "October", "February"], correct: 1 },
+    { question: "Comment dit-on 'Juillet' en anglais ?", answers: ["September", "April", "July", "October"], correct: 2 },
+    { question: "Comment dit-on 'Août' en anglais ?", answers: ["October", "September", "June", "August"], correct: 3 },
+    { question: "Comment dit-on 'Semptembre' en anglais ?", answers: ["June", "October", "September", "December"], correct: 2 },
+    { question: "Comment dit-on 'Octobre' en anglais ?", answers: ["October", "July", "February", "April"], correct: 0 },
+    { question: "Comment dit-on 'Novembre' en anglais ?", answers: ["October", "September", "February", "November"], correct: 3 },
+    { question: "Comment dit-on 'Décembre' en anglais ?", answers: ["September", "October", "December", "Tuesday"], correct: 2 },
+    { question: "Comment dit-on 'Un' en anglais ?", answers: ["Three", "Two", "Four", "One"], correct: 3 },
+    { question: "Comment dit-on 'Deux' en anglais ?", answers: ["Three", "Two", "Four", "One"], correct: 1 },
+    { question: "Comment dit-on 'Trois' en anglais ?", answers: ["Three", "Two", "Four", "One"], correct: 0 },
+    { question: "Comment dit-on 'Quatre' en anglais ?", answers: ["Three", "Two", "Four", "One"], correct: 2 },
+    { question: "Comment dit-on 'Cinq' en anglais ?", answers: ["Three", "Five", "Fifteen", "One"], correct: 1 },
+    { question: "Comment dit-on 'Six' en anglais ?", answers: ["Sixteen", "Two", "Seventeen", "Six"], correct: 3 },
+    { question: "Comment dit-on 'Sept' en anglais ?", answers: ["Three", "Seventeen", "Seven", "Six"], correct: 2 },
+    { question: "Comment dit-on 'Huit' en anglais ?", answers: ["Eighteen", "Eight", "Eighty", "Eleven"], correct: 1 },
+    { question: "Comment dit-on 'Neuf' en anglais ?", answers: ["Nine", "Eighteen", "Nineteen", "Ninety"], correct: 0 },
+    { question: "Comment dit-on 'Dix' en anglais ?", answers: ["Three", "Thirty", "Ten", "Thirteen"], correct: 2 },
+    { question: "Comment dit-on 'Onze' en anglais ?", answers: ["Twelve", "Eight", "Fifty", "Eleven"], correct: 3 },
+    { question: "Comment dit-on 'Douze' en anglais ?", answers: ["Twelve", "Eight", "Fifty", "Eleven"], correct: 0 }
 ];
 function getRandomQuestions(num) {
   const copy = [...questions]; // On fait une copie du tableau original
